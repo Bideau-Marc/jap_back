02b49d6555a9141891846b1148703696eb2e0213
marc.bideau <marc.bideau9@gmail.com>
2023-09-04 20:57:15 +0200

    modif session

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as session from 'express-session';
+import * as sessession from 'express-session';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
@@ -9,7 +9,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.use(
-    session({
+    sessession({
       secret: 'my-secret',
       resave: false,
       saveUninitialized: false,
